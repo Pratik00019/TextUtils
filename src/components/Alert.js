fd068ca0ffd364446998446cd761be93e1b5c35f
Pratik Padalkar <padalkarpratik19@gmail.com>
2022-09-17 18:39:08 +0530

    Final commit

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,18 +1,17 @@
 import React from 'react'
-
 export default function Alert(props) {
+  
     const capital=(word)=>{
         const low=word.toLowerCase();
         return low.charAt(0).toUpperCase()+low.slice(1);
     }
 
- return (
-    // if props.alert is not null then only it will display it
-    props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-        <strong>{capital(props.alert.type)}</strong>:{props.alert.msg}
-  {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"> */}
-  {/* </button> */}
-</div>
-  )
-}
+    return (
+      <div style={{height: '57px'}}>
+      {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`}  role="alert">
+         <strong>{capital(props.alert.type)}</strong>: {props.alert.msg} 
+      </div>}
+      </div>
+    )
+    }
 
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -8,12 +8,16 @@ export default function TextArea(props) {
         let newText=text.toUpperCase();
         setText(newText);
         console.log("upper")
-        props.showAlert("Converted to Upper Case!","success");
+        if(text.length!==0){
+            props.showAlert("Converted to Upper Case!","success");
+        }
     }
     const handleonclicklower=()=>{
         let newText=text.toLowerCase();
         setText(newText);
-        props.showAlert("Converted to Lower Case!","success");
+        if(text.length!==0){
+            props.showAlert("Converted to Lower Case!","success");
+        }
     }
     const handleoncapital=()=>{
         let e=""
@@ -32,7 +36,9 @@ export default function TextArea(props) {
               e+=" ";
         });
         setText(e);
-        props.showAlert("First letters have been capitalized!","success");
+        if(text.length!==0){
+            props.showAlert("First letters have been capitalized!","success");
+        }
     }
 
    const handleonchange=(event)=>{
@@ -42,7 +48,9 @@ export default function TextArea(props) {
    const clearText=()=>{
     let a="";
     setText(a);
-     props.showAlert("Text Cleared!","success");
+    if(text.length!==0){
+        props.showAlert("Text Cleared!","success");
+    }
    }
 
    const copytext=()=>{
@@ -50,50 +58,53 @@ export default function TextArea(props) {
     var text=document.getElementById("my_box");
     text.select();
     navigator.clipboard.writeText(text.value);
-    props.showAlert("Copied to clipboard!","success");
+    document.getSelection().removeAllRanges();
+    if(text.length!==0){
+        props.showAlert("Copied to clipboard!","success");
+    }
    }
     const [text,setText]=useState("");
 
-    let len =text.split(" ").length;
+    // let len =text.split(" ").length;
 
-    const leng=()=>{
-        let c=0;
-        text.split(" ").forEach(element=>{
-            if(element===""){
-                c+=1;
-            }
-        })
-        return(
-            <div>
-                {len-c} words and {text.length} characters
-                <p>{0.008 * (len-c)} Minutes to read </p>
-            </div>
-        )
-    }
+    // const leng=()=>{
+    //     let c=0;
+    //     text.split(/\s+/).forEach(element=>{
+    //         if(element===""){
+    //             c+=1;
+    //         }
+    //     })
+    //     return(
+    //         <div>
+    //             {len-c} words and {text.length} characters
+    //             <p>{0.008 * (len-c)} Minutes to read </p>
+    //         </div>
+    //     )
+    // }
 
   return (
     
         <>
         <div className="container" style={{color:props.mode==="dark"?"white":"black"}}>
-                <h2>{props.heading}</h2>
-        <textarea className="form-control" value={text} onChange={handleonchange} style={{backgroundColor: props.mode==="dark"?"grey":"white",color:props.mode==="dark"?"white":"black"}} id="my_box"rows="7"></textarea>
+                <h3>{props.heading}</h3>
+        <textarea className="form-control" value={text} onChange={handleonchange} style={{backgroundColor: props.mode==="dark"?"#9b9292":"white",color:props.mode==="dark"?"white":"black"}} id="my_box"rows="7"></textarea>
         <br></br>
-        <button className="btn btn-primary mx-2" style ={mystyle} size ="lg" onClick={handleonclick}>Convert Lower to Upper Case</button>
-        <button className="btn btn-primary mx-2" style={{color:"orange"}}onClick={handleonclicklower}>Convert Upper to Lower Case</button>
-        <button className="btn btn-primary mx-2" style={mystyle}onClick={handleoncapital}>Capitalized Case</button>
-        <button className="btn btn-primary mx-2" style={mystyle} onClick={clearText}>Clear Text</button>
-        <button className="btn btn-primary mx-2" style={mystyle} onClick={copytext}>Copy Text</button>
+        <button className="btn btn-primary mx-2 my-1 " style ={mystyle} size ="lg" onClick={handleonclick}>Convert Lower to Upper Case</button>
+        <button className="btn btn-primary mx-2 my-1" style={{color:"orange"}}onClick={handleonclicklower}>Convert Upper to Lower Case</button>
+        <button className="btn btn-primary mx-2 my-1" style={mystyle}onClick={handleoncapital}>Capitalized Case</button>
+        <button className="btn btn-primary mx-2 my-1" style={mystyle} onClick={clearText}>Clear Text</button>
+        <button className="btn btn-primary mx-2 my-1" style={mystyle} onClick={copytext}>Copy Text</button>
         
         
         </div>
 
         <div className="container my-3"style={{color:props.mode==="dark"?"white":"black"}}>
             <h2>Your Text Summary</h2>
-            {leng()}
-            
+            {text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters
+            <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <p id="id1"></p>
             <h3>Preview</h3>
-            <p>{text.length===0?"First enter some text above to display preview":text}</p>
+            <p>{text.length===0?"Nothing to preview":text}</p>
         </div>
         </>
   )

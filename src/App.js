2a2a0aaef8e3148b78654c4dda16f78af484adb2
Pratik Padalkar <padalkarpratik19@gmail.com>
2022-09-17 19:14:40 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-// import AboutUs from './components/AboutUs';
+import AboutUs from './components/AboutUs';
 import Navbar from './components/Navbar';
  import TextArea from './components/TextArea';
 
-//  import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
+ import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 function App() {
@@ -54,20 +54,19 @@ function App() {
   return (
 
     <>
-    {/* <Router> */}
+    <Router>
     <Navbar title="TextUtils" aboutText="About Us" mode ={mode} toggleMode={toggleMode}></Navbar> 
     <div className="container">
-    <TextArea showAlert={showalert}  heading ="Enter the text to analyze" mode={mode} ></TextArea>
     <Alert alert={alert}></Alert>
     {/* <TextArea showAlert={showalert}  heading ="Enter the text to analyze" mode={mode} ></TextArea> */}
-    {/* <Routes>
+    <Routes>
           <Route exact path="/about" element={<AboutUs/>}>
           </Route>
           <Route exact path="/" element={<TextArea showAlert={showalert}  heading ="TextUtils - Word Counter, Character Counter, Case Converter" mode={mode}></TextArea>}>
           </Route>
-    </Routes> */}
+    </Routes>
     </div>
-    {/* </Router> */}
+    </Router>
     </>
   );
 }
